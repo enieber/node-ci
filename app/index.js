88b4630f42a0a7a6ccf2d1bc051518abf29d7c72
Enieber Cunha <eniebercunha@gmail.com>
2016-05-09 03:14:58 -0300

    add first commit

---
+++ app/index.js
@@ -0,0 +1,21 @@
+const Express = require('express');
+const Http = require('http');
+const Redis = require('redis');
+
+var app = Express();
+
+var client = Redis.createClient(
+  process.env.REDIS_1_PORT_6379_TCP_PORT || 6379,
+  process.env.REDIS_1_PORT_6379_TCP_ADDR || '127.0.0.1'
+);
+
+app.get('/', function(req, res, next) {
+  client.incr('visits', function(err, visits) {
+    if(err) return next(err);
+    res.send('You have viewed this page ' + visits + ' times!');
+  });
+});
+
+Http.createServer(app).listen(process.env.PORT || 3000, function() {
+  console.log('Listening on port ' + (process.env.PORT || 3000));
+});
---
+++ app/test/test.js
@@ -0,0 +1,9 @@
+const Assert = require("assert");
+
+describe('Array', function(){
+  describe('indexOf()', function(){
+    it('should return -1 when the value is not present', function(){
+      Assert.equal(-1, [1,2,3].indexOf(4));
+    });
+  });
+});
